378185dd2dadbb38a7f5425d83d0efa806f40553
ethereal97 <ethereal97@gmail.com>
2020-09-07 18:33:21 +0630

    bug fixes

--- js/_components/nav.js
+++ js/_components/nav.js
@@ -1,8 +1,10 @@
 (function () { 
   let lists = document.querySelectorAll('[data-list=nav]');
+  
   if (! lists) {
     return console.warn('could not find any element with "data-list=nav" attribute.');
   }
+  
   let addList = ({name, href}, actived) => { 
     let anchor = document.createElement('a');
     let li = document.createElement('li');
@@ -13,7 +15,14 @@
     return li;
   }
   
-  fetch('nav.json')
+  var nav_url = 'nav.json';
+  
+  if (location.hostname.includes('github.io')) {
+    var repo = location.pathname.split('/')[0];
+    nav_url = [location.hostname, repo, nav_url].join('/');
+  }
+  
+  fetch(nav_url)
   .then(res => res.json())
   .then(items => { 
     items.forEach(item => { 
